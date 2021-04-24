af5bf75478d346946390767174a0549ed257683f
Simone Bergonzi <simemablak@gmail.com>
2021-04-24 23:37:27 +0200

    Animazioni Player

---
+++ RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D RigidBody;
+    public float MoveSpeed;
+
+    public Animator Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * MoveSpeed;
+
+        Animator.SetFloat("MoveX", RigidBody.velocity.x);
+        Animator.SetFloat("MoveY", RigidBody.velocity.y);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            Animator.SetFloat("LastMoveX", Input.GetAxisRaw("Horizontal"));
+            Animator.SetFloat("LastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
